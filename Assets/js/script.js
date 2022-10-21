8c361e35c64ed1f3c4c5e305a39ba97082222c9e
cbazil114 <113719887+cbazil114@users.noreply.github.com>
2022-10-21 17:33:50 -0400

    Minor additions; some variables

---
+++ Assets/js/script.js
@@ -0,0 +1,8 @@
+const win = document.querySelector(".win");
+const lose = document.querySelector("lose");
+const timer = document.querySelector(".timer");
+const startBtn = document.querySelector(".startbtn");
+const submitBtn = document.querySelector(".submitbtn");
+
+let winCounter = 0;
+let loseCounter = 0;
