9a17567b839f9261befa045823480803140f2d56
cbazil114 <113719887+cbazil114@users.noreply.github.com>
2022-10-23 15:00:45 -0400

    Help setting up arrays from tutor

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,49 @@
-const win = document.querySelector(".win");
-const lose = document.querySelector("lose");
-const timer = document.querySelector(".timer");
-const startBtn = document.querySelector(".startbtn");
-const submitBtn = document.querySelector(".submitbtn");
+const container = document.getElementById("container");
+const startButton = document.querySelector("#start-button");
 
-let winCounter = 0;
-let loseCounter = 0;
+const questions = [{
+    question: "Commonly used data types DO NOT include",
+    possibleAnswers: ["strings", "booleans", "alerts", "numbers"],
+    correctAnswer: "d",
+},
+{
+    question: "The condition in an if / else statement is enclosed within _____.",
+    possibleAnswers: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    correctAnswer: "d",
+},
+{
+    question: "Arrays in JavaScript can be used to store _____.",
+    possibleAnswers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    correctAnswer: "all of the above",
+},
+{
+    question: "String values must be enclosed within _____ when being assigned to variables.",
+    possibleAnswers: ["commas", "curly brackets", "quotes", "parentheses"],
+    correctAnswer: "d",
+},
+{
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    possibleAnswers: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    correctAnswer: "d",
+},
+]
+
+let currentQuestionIndex = 0;
+
+function renderQuiz () {
+    const question = document.createElement("header");
+    question.setAttribute("class", "someName");
+    question.setAttribute("id", "someIdName");
+    question.textContent = questions[currentQuestionIndex].question;
+    container.appendChild(question);
+    // build questions, append questions
+}
+
+function startQuiz () {
+    console.log("Hello");
+    container.innerHTML = "";
+    renderQuiz();
+}
+
+startButton.addEventListener("click", startQuiz);
+
